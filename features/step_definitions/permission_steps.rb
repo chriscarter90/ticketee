9f5cca42fbf8a2240387def445e68ab908db2898
Chris Carter <chris.carter1@ntlworld.com>
2012-03-28 16:36:41 +0100

    Trying to implement the restriction of write access for Tickets

--- features/step_definitions/permission_steps.rb
+++ features/step_definitions/permission_steps.rb
@@ -1,3 +1,11 @@
-Given /^"([^"]*)" can view the "([^"]*)" project$/ do |user, project|
-  Permission.create!(:user => User.find_by_email!(user), :thing => Project.find_by_name!(project), :action => "view")
+Given /^"([^"]*)" can ([^"]*?) ([o|i]n)?\s?the "([^"]*)" project$/ do |user, permission, on, project|
+  Permission.create(:user => User.find_by_email!(user), :thing => find_project(project), :action => "create tickets")
+end
+
+def create_permission(email, object, action)
+  Permission.create(:user => User.find_by_email!(email), :thing => object, :action => action)
+end
+
+def find_project(name)
+  Project.find_by_name!(name)
 end
