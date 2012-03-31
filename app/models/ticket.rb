ab691c4521a86e9f13f3b975e3add755f86880a1
Chris Carter <chris.carter1@ntlworld.com>
2012-03-31 23:00:53 +0100

    Users can now leave comments on tickets

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -5,6 +5,7 @@ class Ticket < ActiveRecord::Base
   validates :description, :presence => true, :length => {:minimum => 10}
   
   has_many :assets
+  has_many :comments
   accepts_nested_attributes_for :assets
   
 end
