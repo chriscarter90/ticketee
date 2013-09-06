30887479cee8c68eaef91322f32813315822e1df
Chris Carter <chris.carter@unboxedconsulting.com>
2013-09-06 20:53:46 +0100

    Re-do the way titles are populated and move buttons on project show page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,12 +6,20 @@ module ApplicationHelper
       end
     end
   end
-  
+
+  def header_title(*parts)
+    unless parts.empty?
+      content_for :header_title do
+        parts.join(" - ") unless parts.empty?
+      end
+    end
+  end
+
   def admins_only(&block)
     block.call if current_user.try(:admin?)
     nil
   end
-  
+
   def authorized?(permission, thing, &block)
     block.call if can?(permission.to_sym, thing) || current_user.try(:admin?)
     nil
