8c95892fee47cc18c903a0ee223602a39d9eb610
Chris Carter <chris.carter@unboxedconsulting.com>
2013-09-07 20:15:57 +0100

    Add back buttons on projects form and show page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,6 +15,11 @@ module ApplicationHelper
     end
   end
 
+  def back_button(text, link)
+    button_text = content_tag("i", "", :class => 'icon-chevron-left') + " Back to #{text}"
+    back = link_to(button_text, link, :class => "btn btn-info")
+  end
+
   def admins_only(&block)
     block.call if current_user.try(:admin?)
     nil
